42207778606e4f6cb4c1df385bf8203ba5bc3d7b
renzo3D <renzopozo@yahoo.com>
2018-10-16 12:17:17 -0700

    create components and implement them into App.js

--- src/App.js
+++ src/App.js
@@ -1,35 +1,19 @@
-import React, { Component } from 'react';
-import GoogleMapReact from 'google-map-react';
- 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
- 
-class SimpleMap extends Component {
-  static defaultProps = {
-    center: {
-      lat: 59.95,
-      lng: 30.33
-    },
-    zoom: 11
-  };
- 
-  render() {
-    return (
-      // Important! Always set the container height explicitly
-      <div style={{ height: '100vh', width: '100%' }}>
-        <GoogleMapReact
-          
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
-        >
-          <AnyReactComponent
-            lat={59.955413}
-            lng={30.337844}
-            text={'Kreyser Avrora'}
-          />
-        </GoogleMapReact>
+//import React, { Component } from 'react';
+import React from 'react';
+import MapLocation from './components/googleMap/MapLocation';
+
+export default class App extends React.Component{
+  render(){
+    return(
+    <div className='container'>
+    
+      <div className="row">
+          <div className="col-12 col-md-12 mb-12">
+            <MapLocation />
+          </div>
       </div>
-    );
+
+    </div>
+    )
   }
-}
- 
-export default SimpleMap;
+};
