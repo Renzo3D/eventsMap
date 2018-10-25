a0609ddea4c67a1bf8222559826c21167a7a022c
renzo3D <renzopozo@yahoo.com>
2018-10-25 16:45:59 -0700

    added search bar

--- src/components/googleMap/MapLocation.jsx
+++ src/components/googleMap/MapLocation.jsx
@@ -17,19 +17,16 @@ export default class mapLocation extends React.Component {
   render() {
     return (
       // Important! Always set the container height explicitly
+     
+      
       <div style={{ height: '100vh', width: '100%' }}>
-        <GoogleMapReact
+        <input type='text' placeholder='event' id='event' style={{width:'99.7%'}}></input>
+        <GoogleMapReact defaultCenter={this.props.center} defaultZoom={this.props.zoom}>
           
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
-        >
-          <AnyReactComponent
-            lat={59.955413}
-            lng={30.337844}
-            text={'Kreyser Avrora'}
-          />
+          <AnyReactComponent lat={59.955413} lng={30.337844} text={'Kreyser Avrora'}/>
         </GoogleMapReact>
-      </div>
+        </div>
+      
     );
   }
 }
